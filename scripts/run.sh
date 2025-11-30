8bf40e2be82718b9ba3a6b30b207748cceeae467
Elias Zolotas <ilizol@yahoo.gr>
2025-11-30 14:37:19 +0200

    Update Hapli Test Files for debug. Update.

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,13 +12,14 @@
 # ~/Projects/neanes/dist/neanes-cli --silent-html ../kontakion_prophet_elias/kontakion_prophet_elias_almouzios.byzx
 
 # tests
-find ../tests -type f -name '*.byzx' -print0 | xargs -0 ~/Projects/neanes/dist/neanes-cli --silent-pdf
+# find ../tests -type f -name '*.byzx' -print0 | xargs -0 ~/Projects/neanes/dist/neanes-cli --silent-pdf
 
 # find ../tests -type f -name '*_almouzios.byzx' -print0 | xargs -0 ~/Projects/neanes/dist/neanes-cli --silent-pdf
 
 # find ../tests -type f -name 'Ison*.byzx' -print0 | xargs -0 ~/Projects/neanes/dist/neanes-cli --silent-pdf
 # find ../tests -type f -name 'NoteIndicators*.byzx' -print0 | xargs -0 .~/Projects/neanes/dist/neanes-cli --silent-pdf
 # find ../tests -type f -name 'Accidentals*.byzx' -print0 | xargs -0 ~/Projects/neanes/dist/neanes-cli --silent-pdf
+find ../tests -type f -name 'Hapli*.byzx' -print0 | xargs -0 ~/Projects/neanes/dist/neanes-cli --silent-pdf
 
 # ~/Projects/neanes/dist/neanes-cli --silent-pdf ../tests/MartyriaFthora_almouzios.byzx
 
